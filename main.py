8526f4deabbcdc0efb44728f9b9781e5d474703b
Dylan Sayers <dylansayers@Dylans-MacBook-Pro-2.local>
2018-01-16 18:55:01 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import json
+import urllib3
+from flask import *
+http = urllib3.PoolManager()
+app = Flask("Steamer")
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/", methods=['POST'])
+def search():
+    userId= request.form['id']
+    userId= userId.upper()
+    return redirect(url_for('hello', name=userId))
+
+
+@app.route('/inventory/<name>')
+def hello(name=None):
+    itemlist = []
+    itemname = []
+
+    res = http.request('GET','http://steamcommunity.com/inventory/'+name+'/730/2?l=english&count=5000')
+    data= json.loads(res.data.decode('utf-8'))
+    if data is None:
+        return render_template('bad.html')
+    else:
+        for x in range(len(data['descriptions'])):
+            itemname.append(data['descriptions'][x]['market_name'])
+            itemlist.append(data['descriptions'][x]['icon_url'])
+
+        count = len(itemlist)
+
+        return render_template('hello.html', name=itemname ,list=itemlist, count=count)
+
+@app.route('/stats/<name>')
+def stats(name=None):
+    statsList = []
+    res = http.request('GET', 'http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=4943F4DBDC56C1DD5560EA8A396B8DD5&steamid='+name)
+    data = json.loads(res.data.decode('utf-8'))
+    for item in range(len(data['playerstats']['stats'])):
+        statsList.append(data['playerstats']['stats'][item]['value'])
+    return render_template('stats.html', stats=statsList)
+
+
+if __name__ == '__main__':
+    app.run()
